1c8576987447611cd1b65343aea50e7f6f61513d
Valentin <glukhov_vy@vk.com>
2022-10-15 22:59:54 +0400

    I fixed the problem with "run configuration"

---
+++ 02_HelloKotlin/main.kt
@@ -0,0 +1,11 @@
+fun main () {
+    /*
+My first homework is done
+ */
+    print("My name is Valentin\n") // using regular expression
+    println("I'm from Samara city")
+    print("I'm 37 age old\n")
+    println("I have no programming experience") // using println function
+    println("Русский язык") // testing Russian language
+    println("Русский язык") // testing Russian language double
+}
