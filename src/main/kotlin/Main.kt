0a194762434e713ed34fa55fef7d8b80c0212a9a
Valentin <glukhov_vy@vk.com>
2022-10-15 20:50:34 +0400

    My first homework is done! =)

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    /*
+    My first homework is done
+     */
+    print("My name is Valentin\n") // using regular expression
+    println("I'm from Samara city")
+    print("I'm 37 age old\n")
+    println("I have no programming experience") // using println function
+}
