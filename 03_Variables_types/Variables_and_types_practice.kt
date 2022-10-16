4191d28009898a8e6dffd2a84252b2f3192b8ad4
Валентин Глухов <glukhov_vy@vk.com>
2022-10-16 14:29:19 +0400

    Init commit

---
+++ 03_Variables_types/Variables_and_types_practice.kt
@@ -0,0 +1,34 @@
+fun main () {
+    val firstName: String = "Valentin"
+    val lastName: String = "Glukhov"
+    var height: Double = 177.0
+    val weight: Float = 110.0F
+    var isChild = height < 150 || weight < 40
+    var info = """
+        First name: $firstName
+        Last name: $lastName
+        Height: $height
+        Weight: $weight
+        Is child? - $isChild
+        """
+    println(info)
+    height = 145.0
+    isChild = height < 150 || weight < 40
+    info = """
+        First name: $firstName
+        Last name: $lastName
+        Height: $height
+        Weight: $weight
+        Is child? - $isChild
+        """
+    println(info)
+    info = """
+        An additional option for using a regular expression
+        First name: $firstName
+        Last name: $lastName
+        Height: $height
+        Weight: $weight
+        Is child? - ${height < 150 || weight < 40}
+        """
+    println(info)
+}
