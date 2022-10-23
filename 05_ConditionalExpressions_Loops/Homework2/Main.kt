e5ba65ef15294c917b580067b6daf879a64f47a6
Валентин Глухов <glukhov_vy@vk.com>
2022-10-24 02:29:19 +0400

    First version

---
+++ 05_ConditionalExpressions_Loops/Homework2/Main.kt
@@ -0,0 +1,28 @@
+fun main () {
+
+    fibonacciSequence(inputNumber())
+
+}
+
+fun inputNumber () : Int {
+    println("Please enter a positive integer:")
+    var n: Int = readln().toIntOrNull() ?: inputNumber()
+    while (n < 0) {
+        println("You didn't enter a positive integer.")
+        println("Please enter a positive integer:")
+        n = readln().toIntOrNull() ?: inputNumber()
+    }
+    return n
+}
+
+fun fibonacciSequence (n: Int) : Unit {
+    println("You entered a number: $n")
+    var fN1: Int = 1
+    var fN2: Int = 0
+    for (f in 0 .. n) {
+        val fN: Int = fN1 + fN2
+        fN2 = fN1
+        fN1 = fN
+        println(fN)
+    }
+}
