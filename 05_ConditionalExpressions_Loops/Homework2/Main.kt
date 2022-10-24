c57a5c5f69b06d9626075896423bf0cac3ffd215
Валентин Глухов <glukhov_vy@vk.com>
2022-10-24 14:11:29 +0400

    Final version)

--- 05_ConditionalExpressions_Loops/Homework2/Main.kt
+++ 05_ConditionalExpressions_Loops/Homework2/Main.kt
@@ -17,10 +17,10 @@ fun inputNumber () : Int {
 
 fun fibonacciSequence (n: Int) : Unit {
     println("You entered a number: $n")
-    var fN1: Int = 1
-    var fN2: Int = 0
+    var fN1 = 1.toBigInteger()
+    var fN2 = 0.toBigInteger()
     for (f in 0 .. n) {
-        val fN: Int = fN1 + fN2
+        val fN = fN1 + fN2
         fN2 = fN1
         fN1 = fN
         if (f == n - 2) println("Fibonacci number: $fN")
