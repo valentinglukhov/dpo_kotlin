7efda9141e7989a1b13a523fc7e5e36939bc6fea
Валентин Глухов <glukhov_vy@vk.com>
2023-01-20 13:34:02 +0400

    Final

--- 12_Exceptions/Wheel.kt
+++ 12_Exceptions/Wheel.kt
@@ -3,18 +3,25 @@ import kotlin.random.Random
 class Wheel() {
     var currentPressure: Double = 0.0
     val permissiblePressure = 1.6..2.5
-    val notCriticalPressure = -10.0..10.0
+    val notCriticalPressure = 0.0..10.0
+    var incorrectPressure: Double = 0.0
 
-    fun pumpUp() {
-        val pumpUpPressure = Random.nextDouble(-5.0, 15.0)
+    fun pumpUp(pumpUpPressure: Double) {
         if (pumpUpPressure in notCriticalPressure) {
             currentPressure += pumpUpPressure
-            println(currentPressure)
-            currentPressure = 0.0
-        } else throw IncorrectPressure()
+        } else {
+            incorrectPressure = pumpUpPressure
+            throw IncorrectPressure()
+        }
     }
 
     fun checkPressure() {
-
+        when (currentPressure) {
+            in -5.0..0.0 -> throw IncorrectPressure()
+            in 10.1..15.0 -> throw IncorrectPressure()
+            in 0.1..1.59 -> throw TooLowPressure()
+            in 2.51..10.0 -> throw TooHighPressure()
+            in permissiblePressure -> print(" Давление в колесах в норме. ")
+        }
     }
 }
--- 12_Exceptions/main.kt
+++ 12_Exceptions/main.kt
@@ -2,15 +2,26 @@ import kotlin.random.Random
 
 fun main() {
     val wheel = Wheel()
-    try {
-        repeat(10) { wheel.pumpUp() }
-    } catch (e: IncorrectPressure) {
-        println("Некорректное давление в колесе")
-    } finally {
-        println("Finally block")
+    val pumpUp = runCatching { wheel.pumpUp(15.0) }
+
+    if (pumpUp.isSuccess) {
+        print("При накачке ${wheel.currentPressure} атмосфер процедура удалась.")
+        val checkPressure = runCatching { wheel.checkPressure() }
+        if (checkPressure.isSuccess) {
+            println("Эксплуатация возможна.")
+        } else {
+            when (checkPressure.exceptionOrNull()) {
+                is IncorrectPressure -> println("При накачке ${wheel.incorrectPressure} атмосфер процедура не удалась.")
+                is TooHighPressure ->
+                    println(" Эксплуатация невозможна — давление превышает нормальное.")
+
+                is TooLowPressure ->
+                    println(" Эксплуатация невозможна — давление ниже нормального.")
+            }
+        }
+    } else {
+        println("При накачке ${wheel.incorrectPressure} атмосфер процедура не удалась.")
     }
-
-
 }
 
 class TooHighPressure : Throwable(message = "Слишком высокое давление в колесе")
