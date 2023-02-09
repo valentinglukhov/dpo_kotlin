e9b02fe6353bfee12e6fc466c3588f5d3ceec675
Валентин Глухов <glukhov_vy@vk.com>
2023-02-10 00:36:19 +0400

    11

---
+++ Test/Coroutines/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        launch {
+            println("11211111711111111")
+            delay(2000)
+            println("1232d21ss1111116721222")
+        }
+    }
+
+}
