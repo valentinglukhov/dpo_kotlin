1c3f84bf44bf3176b339a86c158007bd262be84a
Валентин Глухов <glukhov_vy@vk.com>
2023-02-12 23:25:43 +0400

    Final

--- 13_Coroutines/src/main/kotlin/main.kt
+++ 13_Coroutines/src/main/kotlin/main.kt
@@ -1,15 +1,19 @@
 import kotlinx.coroutines.*
 
-fun main() {
-    runBlocking {
-        val scope = CoroutineScope(this.coroutineContext)
-        scope.launch {
-                for (i in 1..300) {
-                    delay(10)
-                    if (i % 100 == 0) println(".") else print(".")
-                }
-            }
+suspend fun main() {
+    val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        println("")
+    }
+    val scopeJob = Job()
+    val scope = CoroutineScope(exceptionHandler + scopeJob)
 
+    scope.launch {
+        launch {
+            for (i in 1..1000) {
+                delay(100)
+                if (i % 100 == 0) println(".") else print(".")
+            }
+        }
         launch {
             delay(500)
             Fibonacci.take(10000)
@@ -21,11 +25,13 @@ fun main() {
         try {
             withTimeout(3000) {
                 delay(3000)
-                launch { Fibonacci.take(50000) }
+                Fibonacci.take(50000)
             }
         } catch (_: Exception) {
             println("\nПревышен таймаут вычисления.")
+            scopeJob.cancel()
         }
-    }
+    }.join()
+
 }
 
