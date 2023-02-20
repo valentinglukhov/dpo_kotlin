a5bf88ff18a9c7d3b4508182d7b1d86e0894c10d
Валентин Глухов <glukhov_vy@vk.com>
2023-02-20 22:40:05 +0400

    20/02

--- 14_Flow/src/main/kotlin/Main.kt
+++ 14_Flow/src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 fun main() {
-    println("")
+    var sequence = (1..90).toList()
+    var newList = mutableListOf<Int>()
+    println(sequence)
+    var lotteryBag = (1..90).toList()
+    for (i in 1..90) {
+        val secondBag = lotteryBag.minusElement(lotteryBag.random().also {
+            println(it)
+            newList.add(it)
+        })
+        println(newList)
+        println(secondBag)
+        lotteryBag = secondBag
+        println(lotteryBag.isEmpty())
+    }
 }
+//var lotteryBag = generateSequence(1) { if (it == 90) null else it + 1}.toList()
