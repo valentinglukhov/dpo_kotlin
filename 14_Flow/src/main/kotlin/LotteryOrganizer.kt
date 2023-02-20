cefcaa68ec4c21b4264e496e6f5cae967f20325f
Валентин Глухов <glukhov_vy@vk.com>
2023-02-20 23:29:09 +0400

    20/02 23:29

--- 14_Flow/src/main/kotlin/LotteryOrganizer.kt
+++ 14_Flow/src/main/kotlin/LotteryOrganizer.kt
@@ -2,4 +2,11 @@ class LotteryOrganizer (vararg player: LotteryPlayer) {
     private var lotteryBag = (1..90).toList()
     private var newList = mutableListOf<Int>()
 
+    init {
+        for (plr in player) {
+            println(plr.name)
+        }
+    }
+
+
 }
--- 14_Flow/src/main/kotlin/LotteryPlayer.kt
+++ 14_Flow/src/main/kotlin/LotteryPlayer.kt
@@ -1,10 +1,14 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 class LotteryPlayer (name: String) {
+    val name: String = name
     var lotteryTicket = mutableListOf<MutableList<Int>>()
     var firstRow = mutableListOf<Int>()
 
     init {
-        while (firstRow.isEmpty()) {
-            firstRow.add(1)
+        while (firstRow.size < 10) {
+            firstRow.add(Random.nextInt(1..10))
         }
         lotteryTicket.add(firstRow)
     }
