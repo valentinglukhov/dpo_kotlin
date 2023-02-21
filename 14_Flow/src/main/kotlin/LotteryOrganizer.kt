b89579fe6d1191b5f8d848ceb2094eb209e80fd5
Валентин Глухов <glukhov_vy@vk.com>
2023-02-21 21:27:02 +0400

    21.02.23 21:26

--- 14_Flow/src/main/kotlin/LotteryOrganizer.kt
+++ 14_Flow/src/main/kotlin/LotteryOrganizer.kt
@@ -19,4 +19,21 @@ class LotteryOrganizer(vararg player: LotteryPlayer) {
             println("Бочонок")
         }
     }
+
+    fun lotteryBag () {
+        var sequence = (1..90).toList()
+        var newList = mutableListOf<Int>()
+        println(sequence)
+        var lotteryBag = (1..90).toList()
+        for (i in 1..90) {
+            val secondBag = lotteryBag.minusElement(lotteryBag.random().also {
+                println(it)
+                newList.add(it)
+            })
+            println(newList)
+            println(secondBag)
+            lotteryBag = secondBag
+            println(lotteryBag.isEmpty())
+        }
+    }
 }
